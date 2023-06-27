d5030a69f5ab04164778e31b56d96c2b9f3d7e72
kozraf <kozraf@gmail.com>
2023-06-28 00:22:10 +0100

    adding details

---
+++ BusinessTier/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import random
+
+app = Flask(__name__)
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why did the chicken go to the seance? To talk to the other side!",
+    "Why don't some animals play cards? Because they're afraid of cheetahs!"
+]
+
+@app.route('/')
+def home():
+    return {
+        'joke': random.choice(jokes),
+        'words': len(jokes[0].split()),
+        'letters': len(jokes[0].replace(" ", "")),
+        'sentences': len(jokes[0].split("."))
+    }
+
+@app.route('/add', methods=['POST'])
+def add_joke():
+    joke = request.json.get('joke')
+    if joke:
+        jokes.append(joke)
+        return {'message': 'Joke added!'}
+    else:
+        return {'message': 'No joke provided.'}, 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
