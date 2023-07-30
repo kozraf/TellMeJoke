5b82ff1dc93621b7067d89aaf525dcabbb3973da
kozraf <kozraf@gmail.com>
2023-07-30 16:41:30 +0100

    testing GitHub Actions

--- tellmejoke_initial_prep_work.sh
+++ tellmejoke_initial_prep_work.sh
@@ -1,4 +1,4 @@
-# Create NFS directories
+# Create NFS directories for TellMeJoke app
 sudo mkdir -p /srv/nfs/k8s/tellmejoke_app-pv
 sudo chown nobody:nogroup /srv/nfs/k8s/tellmejoke_app-pv
 sudo chmod 777 /srv/nfs/k8s/tellmejoke_app-pv
