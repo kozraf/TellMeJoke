427e307acad2bc3b6977fc985d611daa6e595b86
kozraf <kozraf@gmail.com>
2023-08-23 21:35:52 +0100

    adding db connection handling to app.py

--- BusinessTier/app.py
+++ BusinessTier/app.py
@@ -10,49 +10,46 @@ db_user = "root"
 db_password = "mysecretpassword"
 db_name = "jokes_db"  # Update with the actual database name
 
-# Function to establish a connection to the database
-def connect_to_db():
-    try:
-        connection = mysql.connector.connect(
+app = Flask(__name__)
+CORS(app)  # Enable CORS
+
+@app.route('/getJoke')
+def get_joke():
+    # Retrieve a random joke from the MySQL database
+    conn = mysql.connector.connect(
+        host=db_host,
+        port=db_port,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT joke FROM jokes ORDER BY RAND() LIMIT 1")
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if result:
+        return jsonify({'joke': result[0]})
+    else:
+        return jsonify({'joke': 'No jokes found'})
+
+@app.route('/addJoke', methods=['POST'])
+def add_joke():
+    joke = request.json.get('joke')
+    if joke:
+        # Insert the joke into the MySQL database
+        conn = mysql.connector.connect(
             host=db_host,
             port=db_port,
             user=db_user,
             password=db_password,
             database=db_name
         )
-        return connection
-    except Exception as e:
-        print(f"Error connecting to database: {e}")
-        return None
-
-# Function to close the database connection
-def close_db_connection(connection):
-    if connection:
-        connection.close()
-
-app = Flask(__name__)
-CORS(app)  # Enable CORS
-
-jokes = [
-    "Why don't scientists trust atoms? Because they make up everything!",
-    "Why did the chicken go to the seance? To talk to the other side!",
-    "Why don't some animals play cards? Because they're afraid of cheetahs!"
-]
-
-@app.route('/getJoke')
-def get_joke():
-    return {
-        'joke': random.choice(jokes),
-        'words': len(jokes[0].split()),
-        'letters': len(jokes[0].replace(" ", "")),
-        'sentences': len(jokes[0].split("."))
-    }
-
-@app.route('/addJoke', methods=['POST'])
-def add_joke():
-    joke = request.json.get('joke')
-    if joke:
-        jokes.append(joke)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO jokes (joke) VALUES (%s)", (joke,))
+        conn.commit()
+        cursor.close()
+        conn.close()
         return {'message': 'Joke added!'}
     else:
         return {'message': 'No joke provided.'}, 400
